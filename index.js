fe027471218513c273b8c4644b7f828cdf507ce2
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-25 14:59:33 -0500

    HTML prototype being generated, HTML and CSS remain

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-// to do store inputs from inquirer into an object
+// include file system and inquirer libraries as variables
 let fs = require('fs');
 const inquirer = require('inquirer');
-
+// include class definitions for Manager, Engineer, and Intern
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-// onstart
-// enter the team manager’s name, employee ID, email address, and office number
-// OPTIONALLY
-// add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-
+// declare an array of possible employees 
 let myGlobalPeople = [];
 
+// start with adding a manager for all runthroughs
 const addManager = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            message: "What is your team manager's name?",
+            message: "What is the team manager's name?",
             name: 'mgrName',
         },
         {
@@ -46,20 +39,20 @@ const addManager = () => {
             default: true,
         }
     ])
-        .then(({mgrName, mgrID, mgrEmail, mgrOfficeNum} = response) => { 
-            //myGlobalPeople.push(new Manager ( .. . .. )) 
+        .then(({mgrName, mgrID, mgrEmail, mgrOfficeNum, addPerson} = response) => { 
+            // having destructured the responses, create a manager instance and add it to your people array
             myGlobalPeople.push(new Manager(mgrName, mgrID, mgrEmail, mgrOfficeNum));
-            if (response.addPerson) {
+            // if there are additional team members to be added
+            if (addPerson === true) {
                 addTeamMember();
             } 
+            // all team members are added, create the web page
             else {
                 myWriteFunction();
-                process.exit() 
             }
         })
     }
-
-
+// using similar inquirer prompts to manager, get data for new Engineer or Intern class
 const addTeamMember = () => {
     return inquirer.prompt([
         {
@@ -86,55 +79,50 @@ const addTeamMember = () => {
         },
         {
             type: 'input',
-            message: "What is their github?",
+            message: "What is their GitHub?",
             name: 'engGithub',
             when: answers => answers.employeeType == 'Engineer',
         },
         {
             type: 'input',
-            message: "What is your school?",
+            message: "What is their school?",
             name: 'internSchool',
             when: answers => answers.employeeType == 'Intern',
+        },
+        {
+            type: 'confirm',
+            message: "Add another team member? (hit enter for yes)",
+            name: 'addPerson',
+            default: true,
         }
     ])
-        .then(({employeeType, name, id, email} = addOne) => {
+        .then(({employeeType, name, id, email, engGithub, internSchool, addPerson} = addOne) => {
+            // when statements configure the responses to be relevant to the employee type
+            // use Engineer constructor for an engineer
             if (employeeType == "Engineer") {
-                myGlobalPeople.push(new Engineer(name, id, email, addOne.engGithub));    
+                myGlobalPeople.push(new Engineer(name, id, email, engGithub));    
             }
+            // use Intern constructor for an engineer
             else if (employeeType == "Intern") {
-                myGlobalPeople.push(new Intern(name, id, email, addOne.internSchool));
+                myGlobalPeople.push(new Intern(name, id, email, internSchool));
+            }
+            // keep running through the add team member function until no more people need to be added
+            if (addPerson === true) {
+                addTeamMember();
+            } 
+            else {
+                myWriteFunction();
             }
         })
     }
 
 function myWriteFunction() {
     // use global array and generate the doc using boilerplate card HTML with template literals
-    fs.writeFile('garbage_out.html',
-`<h1 id="project-title">${myGlobalPeople[0]}</h1>\n
-<h1 id="project-title">${myGlobalPeople[1]}</h1>\n
-<h1 id="project-title">${myGlobalPeople[2]}</h1>`, 
+    fs.writeFile('./dist/garbage_out.html',
+`<h1 class="project-title">${myGlobalPeople[0].name} who is a ${myGlobalPeople[0].getRole()}</h1>\n
+<h1 class="project-title">${myGlobalPeople[1].name} who is a ${myGlobalPeople[1].getRole()}</h1>\n
+<h1 class="project-title">${myGlobalPeople[2].name} who is a ${myGlobalPeople[2].getRole()}</h1>`, 
     (err) => err ? console.error(err) : console.log('Check directory for details'));
 }
 
 addManager();
-
-
-// .then((response) => {
-//     // deconstruct response object in order to display information in readme file to be written
-//     let {projectNameInput, projectLicense, mgrOfficeNum, commandLineInstallInput, instructProjectUsage, instructProjectContributions, commandLineTestInput, mgrName, mgrID} = response;
-//     // fs writefile includes filename, file contents, and error handling/success message if write is successful 
-//     fs.writeFile('README_output.md', `<p style='text-align: right;'><a href="#project-title">Back to Top</a></p>`, 
-//     (err) => err ? console.error(err) : console.log('Check directory for details'))
-// });
-
-
-// Use writeFileSync method to use promises instead of a callback function
-
-// // Bonus using writeFileSync as a promise
-// const init = () => {
-// 
-//     // Use writeFileSync method to use promises instead of a callback function
-//     .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
-//     .then(() => console.log('Successfully wrote to index.html'))
-//     .catch((err) => console.error(err));
-// }
